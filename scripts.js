560344d09e2a9158a5b803dc1ec9bd6b332b6d00
Thestickman391 <stickman391@gmail.com>
2020-12-05 16:45:43 +1300

    Working visuals

--- scripts.js
+++ scripts.js
@@ -7,10 +7,10 @@ function start() {
 	var src = context.createMediaElementSource(audio1);
 	var analyser = context.createAnalyser();
 
-	var canvas = document.getElementById("canvas");
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	var ctx = canvas.getContext("2d");
+	var canvas1 = document.getElementById("canvas1Interviewer");
+	canvas1.width = window.innerWidth;
+	canvas1.height = window.innerHeight;
+	var ctx1 = canvas1.getContext("2d");
 
 	src.connect(analyser);
 	analyser.connect(context.destination);
@@ -22,8 +22,8 @@ function start() {
 
 	var dataArray = new Uint8Array(bufferLength);
 
-	var WIDTH = canvas.width;
-	var HEIGHT = canvas.height;
+	var WIDTH = canvas1.width;
+	var HEIGHT = canvas1.height;
 	console.log(HEIGHT);
 
 	var barWidth = (WIDTH / bufferLength) * 2.5;
@@ -37,19 +37,19 @@ function start() {
 
 	  analyser.getByteFrequencyData(dataArray);
 
-	  ctx.clearRect(0,0,canvas.width,canvas.height);
+	  ctx1.clearRect(0,0,canvas1.width,canvas1.height);
 
 	  for (var i = 0; i < bufferLength; i++) {
-		barHeight = dataArray[i] * 2;
+		barHeight = dataArray[i] * 3;
 		
 		var r = 0;
 		var g = 170;
 		var b = 255;
 
-		ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-		ctx.fillRect(x, HEIGHT - barHeight, barWidth, HEIGHT);
+		ctx1.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+		ctx1.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
 
-		x += barWidth + 1;
+		x += barWidth - 1;
 	  }
 	}
 
